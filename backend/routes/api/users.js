e5746675f98bcb0c858803bcaaed2f6c2540914f
paul-overlie <paul.overlie@gmail.com>
2024-04-04 06:53:08 -0500

    fixed user errors

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,12 +33,48 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
+      const { email, password, username, firstName, lastName } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      
+      let emailedUser = await User.unscoped().findOne({
+        where: {email}
+      })
+      // console.log("email", email, "oldEmail", emailedUser)
+      
+      if(emailedUser&&emailedUser.email===email) {
+        let err = {}
+        res.status=500
+        err.message="User already exists"
+        err.errors={
+        email:"User with that email already exists"}
+        console.log(err)
+        res.json(err)
+      }
+
+      let namedUser = await User.unscoped().findOne({
+        where: {username}
+      })
+      console.log("username", username, "namedUsername", namedUser)
+
+      if(namedUser.username===username) {
+        let nameErr={}
+        res.status=400
+        nameErr.message = "Bad Request",
+        nameErr.errors={
+          email: "Invalid email",
+          username: "Username is required",
+          firstName: "First Name is required",
+          lastName: "Last Name is required"
+        }
+        res.json(nameErr)
+      }
+
+      const user = await User.create({ firstName, lastName, email, username, hashedPassword });
   
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username,
       };
