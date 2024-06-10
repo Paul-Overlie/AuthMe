59d9d95406ae44c8de884df581459f0d16dbd037
paul-overlie <paul.overlie@gmail.com>
2024-06-10 03:20:41 -0500

    finished updating group

--- frontend/src/components/GroupList/EditGroup.jsx
+++ frontend/src/components/GroupList/EditGroup.jsx
@@ -52,52 +52,52 @@ export const EditGroup = () => {
     
 
 
-    return <form onSubmit={(e)=>{onSubmit(e)}}>
-        <div>Update your Group</div>
+    return <div className="CreateGroupFormContainer"><form className="CreateGroupForm" onSubmit={(e)=>{onSubmit(e)}}>
+        <div className="CreateGroupTitle">Update your Group</div>
         <div className="createGroup1">
-            <div>{"Set your group's location"}</div>
-            <div>{"Meetup groups meet locally, in person, and online. We'll connect you with people in your area."}</div>
-            <input type="text" placeholder="City, STATE" 
+            <div className="createGroup1Command">{"Set your group's location"}</div>
+            <div className="CreateGroup1Description">{"Meetup groups meet locally, in person, and online. We'll connect you with people in your area."}</div>
+            <input className="createGroup1Input" type="text" placeholder="City, STATE" 
             value={location}
             onChange={(e)=>setLocation(e.target.value)}/>
         </div>
         {errors?.city ? <div>{errors.city}</div> : null}
         {errors?.state ? <div>{errors.state}</div> : null}
-        <div className="createGroup2">
-            <div>{"What will your group's name be?"}</div>
+        <div className="createGroup1">
+            <div className="createGroup1Command">{"What will your group's name be?"}</div>
             <div>{`Chose a name that will give people a clear idea of what the group is about. Feel free to get creative! You can edit this later if you change your mind.`}</div>
-            <input type="text" placeholder="What is your group name?"
+            <input type="text" className="createGroup1Input" placeholder="What is your group name?"
             value={name}
             onChange={(e)=>setName(e.target.value)}/>
         </div>
         {errors?.name ? <div>{errors.name}</div> : null}
-        <div className="createGroup3">
-            <div>Describe the purpose of your group.</div>
+        <div className="createGroup1">
+            <div className="createGroup1Command">Describe the purpose of your group.</div>
             <div>{`People will see this when we promote your group, but you'll be able to add to it later, too. 1. What's the purpose of the group? 2. Who should join? 3. What will you do at your events?`}</div>
-            <textarea placeholder="Please write at least 30 characters." 
+            <textarea className="createGroup1Input" placeholder="Please write at least 30 characters." 
             value={about}
             onChange={(e)=>setAbout(e.target.value)}/>
         </div>
         {errors?.about ? <div>{errors.about}</div> : null}
-        <div className="createGroup4">
-            <label>Is this an in-person or online group?
-                <select value={online} onChange={(e)=>setOnline(e.target.value)}>
+        <div className="createGroup1">
+            <label className="createGroup4" htmlFor="online">Is this an in-person or online group?</label>
+                <select className="createGroup4Input" name="online" value={online} onChange={(e)=>setOnline(e.target.value)}>
                     <option>In Person</option>
                     <option>Online</option>
                 </select>
-            </label>
-            <label>Is this group private or public?
-                <select value={publicity} onChange={(e)=>setPublicity(e.target.value)}>
+            
+            <label className="createGroup4" htmlFor="private">Is this group private or public?</label>
+                <select className="createGroup4Input" name="private" value={publicity} onChange={(e)=>setPublicity(e.target.value)}>
                     <option value={true}>Private</option>
                     <option value={false}>Public</option>
                 </select>
-            </label>
-            <label>Please add an image URL for your group below.
-                <input type="text" placeholder="Image Url"
+            
+            <label className="createGroup4" htmlFor="image">Please add an image URL for your group below.</label>
+                <input name="image" className="createGroup4Input" type="text" placeholder="Image Url"
                 value={image}
                 onChange={(e)=>setImage(e.target.value)}/>
-            </label>
+            
         </div>
         <button>Update Group</button>
-    </form>
+    </form></div>
 }
